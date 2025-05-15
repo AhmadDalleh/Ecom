0cebf1102019d1b3342b7f411d8bd250df597a72
AhmadDalleh <ahmeddalleh8@gmail.com>
2025-05-15 16:52:17 +0300

    Add the Update and Add and Delete end points and auto mapper

--- Ecom.API/Controllers/CategoriesController.cs
+++ Ecom.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Ecom.Core.Interfaces;
+﻿using AutoMapper;
+using Ecom.Core.DTOs;
+using Ecom.Core.Entities.Product;
+using Ecom.Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,23 +11,83 @@ namespace Ecom.API.Controllers
     [ApiController]
     public class CategoriesController : BaseController
     {
-        public CategoriesController(IUnitOfWork work) : base(work)
+        public CategoriesController(IUnitOfWork work, IMapper mapper) : base(work, mapper)
         {
         }
+
         [HttpGet("get-all")]
         public async Task<IActionResult> get()
         {
             try
             {
                 var category = await work.CategoryRepository.GetAllAsync();
-                if (category is null) 
+                if (category is null)
                     return BadRequest();
                 return Ok(category);
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
+        [HttpGet("get-by-id/{id}")]
+        public async Task<IActionResult> getbyId(int id)
+        {
+            try
+            {
+                var category = await work.CategoryRepository.GetByIdAsync(id);
+                if (category is null) return BadRequest();
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("add-category")]
+        public async Task<IActionResult> add(CategoryDTO categoryDTO)
+        {
+            try
+            {
+                var category = mapper.Map<Category>(categoryDTO);
+                await work.CategoryRepository.AddAsync(category);
+                return Ok(new { message = "Item has been Added" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("update-category")]
+        public async Task<IActionResult> update(UpdateCategoryDTO categoryDTO)
+        {
+            try
+            {
+                var category = mapper.Map<Category>(categoryDTO);
+                await work.CategoryRepository.UpdateAsync(category);
+                return Ok(new { message = "Item has been updated" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("delete-category/{id}")]
+        public async Task<IActionResult> delete(int id)
+        {
+            try
+            {
+                await work.CategoryRepository.DeleteAsync(id);
+                return Ok(new { message = "item has been deleted" });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
---
+++ Ecom.API/Controllers/Mapping/CategoryMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Ecom.Core.DTOs;
+using Ecom.Core.Entities.Product;
+using System.Runtime;
+
+namespace Ecom.API.Controllers.Mapping
+{
+    public class CategoryMapping:Profile
+    {
+        public CategoryMapping()
+        {
+            CreateMap<CategoryDTO,Category>().ReverseMap();
+            CreateMap<UpdateCategoryDTO,Category>().ReverseMap();
+        }
+    }
+}
