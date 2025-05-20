0524eb7d02e7c66c67ff15b9ef7a239aee11ca46
AhmadDalleh <ahmeddalleh8@gmail.com>
2025-05-20 15:47:54 +0300

    End Seurty Sction

--- Ecom.API/Program.cs
+++ Ecom.API/Program.cs
@@ -1,3 +1,4 @@
+using Ecom.API.Middleware;
 using Ecom.infrastructure;
 namespace Ecom.API
 {
@@ -8,12 +9,13 @@ namespace Ecom.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddMemoryCache();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.infrastructureConfiguration(builder.Configuration);
+
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             var app = builder.Build();
@@ -24,7 +26,8 @@ namespace Ecom.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
